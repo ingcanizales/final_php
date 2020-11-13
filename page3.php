0c36d196d25ce8dc5c2452f0fcaa04beb71be831
jimmy de jesus cañizales <ingcanizales2793@gmail.com>
2020-11-12 19:09:41 -0500

    se realiza ejercicio final

---
+++ page3.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ejemplo Responsive 2</title>
+    <link rel="stylesheet" type="text/css" href="css/estilos.css">
+</head>
+<body>
+    <div class="container">
+
+        <header>
+            <h1>DEPORTES MAS POPULARES</h1>
+        </header>
+
+            <nav>
+                <ul>
+                    <li id="menu1"><a href="index.php">FUTBOL</a></li>
+                    <li id="menu2"><a href="page2.php">BASQUETBOL</a></li>
+                    
+                    
+                </ul>
+                <ul>
+                <li id="menu3" class="activo"><a href="page3.php">BEISBOL</a></li> 
+                </ul>
+            </nav>
+
+            <section>
+                <p>
+                El beisbol es un deporte en el que dos equipos compuestos por nueve  jugadores se enfrentan entre sí. Cada equipo, que dispone de nueve  jugadores que se mueven por el campo tratando de atrapar y sacar los tres outs de cada entrada y tratando de  anotar mas carreras que el equipo rival. 
+                </p>
+                <p>    
+                Beisbol  este deporte, que en Inglaterra  también recibe el nombre de Runders  y que en los Estados Unidos se conoce como baseball, tiene como regla más importante que los jugadores, no traten de golpearse con sus bates o con la pelota, cada vez que un jugador pisa el home en el momento que su rival esta recibiendo en el campo  cuenta como una carrera, 
+                </p>
+                <p>
+                cuando el jugador batea lo único que tiene que hacer es que no le atrapen su batazo  o que lo saquen en una de las cuatro bases permitidas (primera, segunda, tercera y home) solo que el jugador la saque del campo se marcara como homeruun. 
+                </p>
+                <p>
+                El campo de beisbol  tiene una forma de diamante el cual se dispersan los nueve jugadores y cada uno toma uno de las nueve posiciones (Lanzador, Receptor, Primera base, Segunda base, Tercera base, parador en corto, Jardinero izquierdo, Jardinero central, Jardinero derecho) cada uno tiene su propia tarea de hacer su outs ya sean los filders (Jardinero izquierdo, Jardinero central, Jardinero derecho)que sus outs son elevados y los del cuadro (Lanzador, Receptor, Primera base, Segunda base, Tercera base, parador en corto) que los suyos son a primera todos los lanzamientos o solo que sea un doble play.
+                </p>
+                <p>
+                Cada año se forma la liga mundial de beisbol mejor conocida como la MLB donde todos los equipos que tienen un mejor promedio de juego son invitados y son clasificados para la contienda algunos de los equipos que por lo regular entran a este campeonato son MEXICO, U.S.A, JAPON, CANADA etc. Es un gran alborotó para los aficionados y una gran entretenimiento.
+                </p>
+            </section>
+            <aside>
+                BALÓN DE BEISBOL
+                <br>
+                <img class="tamano" src="images/beisbol3.jpg">
+            </aside>
+            <footer>
+                <p>
+                © Noviembre 2020 Unidad Editorial Información Deportiva, S.L.U. Todos los derechos reservados.
+                </p>
+            </footer>
+    </div>
+</body>
+</html> 
